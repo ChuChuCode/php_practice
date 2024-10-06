ffc615048ad66e8490638f4f08bd1275db89a425
ChuChu_Code <zero789456321@gmail.com>
2024-10-07 02:38:05 +0900

    Add nav and css file

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Title</title>
+<link rel="stylesheet" href="static/nav.css">
+<title>ChuChu</title>
 </head>
 
 <body>
+    <?php
+        // Should put into header.php
+    ?>
+    <nav>
+        <a href="index.php">
+            <img src="static/ChuChu.png" alt="Icon" width="30" height="30"/>
+        </a>
+        <a href="#">Home</a>
+        <a href="#">Collection</a>
+        <a href="#">About me</a>
+    </nav>
     <form method="GET">
         <input type="text" name="person">
         <button>SUBMIT</button>
