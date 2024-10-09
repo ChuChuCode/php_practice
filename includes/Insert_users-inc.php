bbc7f191720ecf6a8c2e2bcc04a87c4a37c8d962
ChuChu_Code <zero789456321@gmail.com>
2024-10-09 23:36:55 +0900

    Use Prepared Statements
    Prepare statement for indert and select

--- includes/Insert_users-inc.php
+++ includes/Insert_users-inc.php
@@ -1,14 +1,26 @@
 <?php
 	include_once "dbconn-inc.php";
+	// Prevent from sql injection
+	$first = mysqli_real_escape_string($conn,$_POST['first']);
+	$last = mysqli_real_escape_string($conn,$_POST['last']);
+	$email = mysqli_real_escape_string($conn,$_POST['email']);
+	$UserID = mysqli_real_escape_string($conn,$_POST['UserID']);
+	$pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
 
-	$first = $_POST['first'];
-	$last = $_POST['last'];
-	$email = $_POST['email'];
-	$UserID = $_POST['UserID'];
-	$pwd = $_POST['pwd'];
-
-	$sql = "INSERT INTO users (user_first,user_last,user_email,user_userid,user_pwd) VALUES ('$first','$last','$email','$UserID','$pwd');";
+	/*
+	$sql = "INSERT INTO users (user_first,user_last,user_email,user_userid,user_pwd) VALUES 
+		('$first','$last','$email','$UserID','$pwd');";
 
 	mysqli_query($conn,$sql);
-
+	*/
+	//Use Prepared Statements
+	$sql = "INSERT INTO users (user_first,user_last,user_email,user_userid,user_pwd) VALUES 
+		(?,?,?,?,?);";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt,$sql)){
+		echo "SQL Error!!";
+	} else {
+		mysqli_stmt_bind_param($stmt, "sssss", $first,$last,$email,$UserID,$pwd);
+		mysqli_stmt_execute($stmt);
+	}
 	header("Location: ../practice.php/?signup=success");
--- includes/dbconn-inc.php
+++ includes/dbconn-inc.php
@@ -17,3 +17,21 @@
             }
         }
     }
+    // Use Prepared Statements
+    function userList_Statement($conn){
+        $data = "kerwinchu";
+        $sql = "SELECT * FROM users WHERE user_userid = ?;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt,$sql)){
+            echo "SQL statement failed!";
+        } else {
+            // s = string, i = integer, b = BLOB, d = double 
+            mysqli_stmt_bind_param($stmt, "s", $data);
+            // Run database
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row["user_userid"];
+            }
+        }
+    }
